42741a181ed3ef3485235532a10290435c38766c
TazmeenM <tazmeen.mohsin@gmail.com>
2025-09-07 19:29:17 -0400

    Added Prizes to the Treasure Chest
    Added a way to give players prizes in the form of health and potions once they open the treasure chest, and created a label to tell players what they earned from the chest.

--- scripts/treasure_chest.gd
+++ scripts/treasure_chest.gd
@@ -1,8 +1,17 @@
 extends Area2D
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var item_earned_label: Label = $ItemEarnedLabel
+@onready var item_earned_label_timer: Timer = $ItemEarnedLabelTimer
 
+static var treasureChestOpened = false
 var treasureChestAreaEntered = false
+var labelIndex = 0
+
+var treasureChestItems = ["+4 Health Potion", "+2 Speed Potion", "Health Filled"]
+
+func _ready() -> void:
+	item_earned_label.visible = false
 
 
 func _process(delta: float) -> void:
@@ -23,3 +32,18 @@ func _on_body_exited(body: Node2D) -> void:
 
 func openTreasureChest() -> void:
 	sprite_2d.texture = load("res://assets/sprites/treasureChestOpened.png")
+	treasureChestOpened = true
+	Inventory.addPotions(2, "speedPotion")
+	Inventory.addPotions(4, "healthPotion")
+	Inventory.health = 100
+	item_earned_label.visible = true
+	item_earned_label_timer.start()
+	
+
+
+func _on_items_earned_label_timer_timeout() -> void:
+	item_earned_label.visible = true
+	labelIndex += 1
+	if (labelIndex < treasureChestItems.size()):
+		item_earned_label.text = treasureChestItems[labelIndex]
+	
